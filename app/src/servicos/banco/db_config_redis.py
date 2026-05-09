9deff1e0ddd1c6458ca8443903d2d41e30288c5d
rodrigo <silva.rodrigo31@gmail.com>
2026-05-08 21:32:55 -0300

    feat: criação env

--- app/src/servicos/banco/db_config_redis.py
+++ app/src/servicos/banco/db_config_redis.py
@@ -2,16 +2,14 @@ from typing import Dict
 
 from redis import Redis
 
-from servicos.banco.idb_config import IdbConfig, DRIVER
+from servicos.banco.idb_config import IdbConfig
 
 
 class DbConfigRedis(IdbConfig[Redis]):
 
-    def __init__(self, driver: DRIVER):
-        pass
-
     def obter_parametros_conexao(self) -> Dict[str, str]:
         pass
 
-    def obter_driver(self) -> DRIVER:
-        pass
+    def obter_driver(self) -> Redis:
+        driver = Redis(host='localhost', port=6379, db=0)
+        return driver
